102728c24abfb98800b07cc1e16ecd22392e53b9
korovindenis <korovindenis@yandex.ru>
2023-06-11 22:19:16 +0300

    exit codes

--- cmd/sfb/main.go
+++ cmd/sfb/main.go
@@ -9,6 +9,11 @@ import (
 	"github.com/spf13/viper"
 )
 
+const (
+	EXIT_SUCCESS = 0
+	EXIT_ERROR   = 1
+)
+
 // @title           Shutdown from browser
 // @version         0.1
 // @description     Linux service for shutdown PC from the browser (Go, React)
@@ -21,7 +26,7 @@ import (
 func main() {
 	if err := config.Init(); err != nil {
 		log.Printf("%s", err.Error())
-		os.Exit(1)
+		os.Exit(EXIT_ERROR)
 	}
 	port := viper.GetString("port")
 	logslevel := viper.GetUint("logslevel")
@@ -29,11 +34,11 @@ func main() {
 	sfb, err := transport.NewSfb(logslevel)
 	if err != nil {
 		log.Printf("%s", err.Error())
-		os.Exit(1)
+		os.Exit(EXIT_ERROR)
 	}
 	if err := sfb.Run(port, logslevel); err != nil {
 		log.Printf("%s", err.Error())
-		os.Exit(1)
+		os.Exit(EXIT_ERROR)
 	}
-	os.Exit(0)
+	os.Exit(EXIT_SUCCESS)
 }
