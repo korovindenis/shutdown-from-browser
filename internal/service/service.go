cf99bdfac4c2cde77c26193169a14c1d288f4912
korovindenis <korovindenis@yandex.ru>
2023-06-12 16:29:46 +0300

    build on windows

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,6 +1,9 @@
 package service
 
-import "time"
+import (
+	"log"
+	"time"
+)
 
 type Status struct {
 	Mode         string
@@ -12,6 +15,37 @@ type countdown struct {
 	h int
 	m int
 	s int
+}
+
+func New(s *Status, logslevel uint) {
+	for {
+		if s.Mode == "" {
+			time.Sleep(time.Second * 5)
+		} else {
+			time.Sleep(1 * time.Second)
+			v, _ := time.Parse(time.RFC3339, s.TimeShutDown)
+			timeRemaining := getTimeRemaining(v)
+
+			if s.Mode != "" {
+				if timeRemaining.t <= 0 {
+					// bye
+					if logslevel > 0 {
+						log.Println("Run:", s.Mode)
+					}
+					callMode := syscall.LINUX_REBOOT_CMD_POWER_OFF
+					if s.Mode == "reboot" {
+						callMode = syscall.LINUX_REBOOT_CMD_RESTART
+					}
+					err := syscall.Reboot(callMode)
+					if err != nil {
+						log.Fatalf("%s", err)
+					}
+				} else if logslevel > 1 {
+					log.Printf("Time for %s - %d:%d:%d\n", s.Mode, timeRemaining.h, timeRemaining.m, timeRemaining.s)
+				}
+			}
+		}
+	}
 }
 
 func getTimeRemaining(t time.Time) countdown {
